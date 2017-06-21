ebb446b9a2e9775fd0a32f9e17cd5625ac3bf03a
Andrew Leykin <katashi1328@mail.ru>
2017-06-21 16:27:52 +0300

    css и js в один файл, изменил стили всех страниц

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -4,9 +4,10 @@
 
 module.exports = function() {
   $.gulp.task('sass', function() {
-    return $.gulp.src('./source/style/pages/*.scss')
+    return $.gulp.src('./source/style/*.scss')
       .pipe($.gp.sourcemaps.init())
       .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
+      .pipe($.gp.csso())
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
       .pipe($.gp.sourcemaps.write())
       .pipe($.gulp.dest($.config.root + '/app/css'))
