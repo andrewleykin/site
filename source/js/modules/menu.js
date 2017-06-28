1b35af2e6b8018160b65b6388d254c562a602eab
Andrew Leykin <katashi1328@mail.ru>
2017-06-28 18:54:14 +0300

    parallax4

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -34,4 +34,35 @@
   	},700);
 
   });
+
+  var parallax = (function () {
+  var bg = $('.welcome__bg__img');
+  var user = $('.user-block');
+  var svgPortfolio = $('.svg-bg__portfolio_header');
+  var svgBlog = $('.svg-bg__blog_header');
+
+  return {
+    move: function(block, windowScroll, strafeAmount) {
+      var strafe = windowScroll / -strafeAmount + '%';
+      var transformString = 'translate3d(0,' + strafe + ',0)';
+      
+      block.css.transform = transformString;
+
+      console.log(block.css.transform);
+    },
+    init: function (wScroll) {
+      this.move(bg, wScroll, 45);
+      this.move(svgPortfolio, wScroll, 15);
+      this.move(svgBlog, wScroll, 15);
+      this.move(user, wScroll, 2);
+    }
+  }
+
+}());
+
+window.onscroll = function () {
+  var wScroll = window.pageYOffset;
+
+  parallax.init(wScroll);
+}
 })();
