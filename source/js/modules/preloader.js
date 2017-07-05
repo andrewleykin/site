acd72f7ab0ed4438ff0ad1e6bb5e2706f75c6742
Andrew Leykin <katashi1328@mail.ru>
2017-07-05 21:07:28 +0300

    promise для preloader

--- source/js/modules/preloader.js
+++ source/js/modules/preloader.js
@@ -1,7 +1,6 @@
 // js файл для прелоадера на любых страницах
 
-$(function(){
-
+	// задаём переменные
 	var images = $('img'),
 		imagesTotalCount = images.length,
 		imagesLoadedCount = 0,
@@ -10,34 +9,60 @@ $(function(){
 		rounds = $('.preloader__rounds'),
 		strokeGlobal = 450,
 		strokeStart = 450,
-		mainStroke;
+		strokeDashoffset;
 
+	// промис который будет проверять наличие прелоадера на странице
+	var preloaderPromise = new Promise (function(resolve, reject) {
+			if (preloader.length) {
+				resolve();
+			} else {
+				reject();
+			}
+		});
 
+	// функция при наличии прелоадера на странице
+	preloaderPromise.then(function(){
 
-	for (var i=0; i < imagesTotalCount; i++) {
-		imageClone = new Image();
-		imageClone.onload = imageLoaded;
-		imageClone.onerror = imageLoaded;
-		imageClone.src = images[i].src;
-	}
-
-	function imageLoaded() {
-		imagesLoadedCount++;
-		var perc = Math.round(((100 / imagesTotalCount) * imagesLoadedCount)) + '%';
-		mainStroke = strokeStart - Math.round((strokeGlobal / imagesTotalCount));
-		strokeStart -= (strokeGlobal / imagesTotalCount);
-
-		console.log(mainStroke);
-		rounds.css('strokeDashoffset', mainStroke);
-
-		percDisplay.html(perc);
-		
-		if(imagesLoadedCount >= imagesTotalCount) {
-			setTimeout(function(){
-				if(!preloader.hasClass('done')){
-					preloader.addClass('done');
-				}
-			}, 1000);
+		// цикл для перебирания всех картинок
+		for (var i=0; i < imagesTotalCount; i++) {
+			imageClone = new Image();
+			imageClone.onload = imageLoaded;
+			imageClone.onerror = imageLoaded;
+			imageClone.src = images[i].src;
 		}
-	}
-});
+
+		// функция для проверки загрузки всех картинок
+		function imageLoaded() {
+
+			// увеличиваем число загруженных картинок
+			imagesLoadedCount++;
+
+			// считаем процент загруженных
+			var perc = Math.round(((100 / imagesTotalCount) * imagesLoadedCount)) + '%';
+			
+			// выводим наше значение процентное
+			percDisplay.html(perc);
+
+			// считаем относительное закраску обводки круга
+			strokeDashoffset = strokeStart - Math.round((strokeGlobal / imagesTotalCount));
+
+			// вычитаем стартовый отчёт
+			strokeStart -= (strokeGlobal / imagesTotalCount);
+
+			// присваиваем то что посчитали, нашему кругу свг
+			rounds.css('strokeDashoffset', strokeDashoffset);
+
+			// Если все картинки загруженны, убрать блок прелоадер
+			if(imagesLoadedCount >= imagesTotalCount) {
+				setTimeout(function(){
+					if(!preloader.hasClass('done')){
+						preloader.addClass('done');
+					}
+				}, 1000);
+			}
+		}
+	}).catch(function(){
+		return ;
+		});
+
+
