a1385f0c178e537d1f9fdc1a6aff239c7590243c
Andrew Leykin <katashi1328@mail.ru>
2017-07-09 23:13:04 +0300

    add slider display

--- source/js/pages/works/slider__controls.js
+++ source/js/pages/works/slider__controls.js
@@ -3,16 +3,46 @@
 
 $(function(){
 
+	const btnPrev = $('.slider__prev');
+	const btnNext = $('.slider__next');
 	const duration = 500;
-	let counter    = 1;
+	let active = 'slider-controls__item-active';
 	let inProgress = false;
 
-	const moveSlides = (container, direction) => {
-		let items      = container.find('.slider-controls__item');
-		let activeItem = items.filter('.active');
-		let strafePerc = direction === 'down' ? 100 : -100;
+	const moveSlidesPrev = (container, direction) => {
+		let items        = container.find('.slider-controls__item');
+		let activeItem   = items.filter('.slider-controls__item-active');
+		let strafePerc   = direction === 'down' ? 100 : -100;
+		let counter      = activeItem.index();
 
-		console.log(items);
+		counter--;
+
+		if(counter < 0) counter = items.length - 1;
+
+		const reqItem = items.eq(counter);
+
+		activeItem.animate({
+			'top': `${strafePerc}%`,
+		}, duration)
+
+		reqItem.animate({
+			'top': '0',
+		}, duration, function (){
+			activeItem.removeClass(active).css('top', `${-strafePerc}%`);
+			$(this).addClass(active);
+
+			inProgress = false;
+		});
+	}
+
+	const moveSlidesNext = (container, direction) => {
+		let items         = container.find('.slider-controls__item');
+		let activeItem    = items.filter('.slider-controls__item-active');
+		let strafePerc    = direction === 'down' ? 100 : -100;
+		let counter       = activeItem.index();
+
+		counter++;
+
 		if (counter >= items.length) counter = 0;
 
 		const reqItem = items.eq(counter);
@@ -24,24 +54,33 @@ $(function(){
 		reqItem.animate({
 			top: 0
 		}, duration, function (){
-			activeItem.removeClass('active').css('top', `${-strafePerc}%`);
-			$(this).addClass('active');
+			activeItem.removeClass(active).css('top', `${-strafePerc}%`);
+			$(this).addClass(active);
 
 			inProgress = false;
 		});
 	}
 
-	$('.js__slider-controls').on('click', function(e) {
+	btnPrev.on('click', function(e) {
 		e.preventDefault();
 
 
 		if (inProgress) return;
-
 		inProgress = true;
 
-		moveSlides($('.slider__prev'), 'down');
-		moveSlides($('.slider__next'), 'up');
-		counter++;
+		moveSlidesPrev(btnPrev, 'down');
+		moveSlidesPrev(btnNext, 'up');
+	});
+
+	btnNext.on('click', function(e) {
+		e.preventDefault();
+
+
+		if (inProgress) return;
+		inProgress = true;
+
+		moveSlidesNext(btnPrev, 'down');
+		moveSlidesNext(btnNext, 'up');
 	});
 
 });
--- source/js/pages/works/slider__display.js
+++ source/js/pages/works/slider__display.js
@@ -1,1 +1,46 @@
 // js файл для анимации главного дисплея
+
+
+$(function(){
+
+	const btnPrev = $('.slider__prev');
+	const btnNext = $('.slider__next');
+
+
+	const sliderShow = function(container) {
+		let display = container.closest('.slider-right').find('.slider__display-img'),
+			path = container.find('.slider-controls__item-active').children('.slider-controls__img').attr('src'),
+			fadedOut = $.Deferred(),
+			loaded = $.Deferred();
+
+	display.fadeOut(function () {
+		fadedOut.resolve();
+	});
+
+	fadedOut.done(() => {
+		display.attr('src', path).on('load', () => {
+			loaded.resolve();
+		});
+	});
+
+	loaded.done(() => {
+		display.fadeIn(500);
+	});
+
+}
+
+	btnPrev.on('click', function(e) {
+		e.preventDefault();
+
+		sliderShow(btnPrev);
+
+	});
+
+	btnNext.on('click', function(e) {
+		e.preventDefault();
+
+		sliderShow(btnNext);
+
+	});
+
+});
